7df06768d97a5f4e5bbd5d755cd742bfff7d6f23
Jose Miguel <miguel.gamazo@galiclick.com>
2012-03-17 11:46:06 +0100

    Changed unicorn file

--- config/initializers/spree-ini.rb
+++ config/initializers/spree-ini.rb
@@ -1,2 +1,3 @@
+Spree::Config.set(:allow_ssl_in_production => false)
 Spree::Config.set(:site_name => "Galiclick 2.0 - Tu Tienda Online de Marcas Blancas")
 Spree::Config.set(logo: "store/logo-nuevo.gif")
