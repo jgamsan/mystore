b69aeef6a2785440735512d22d861cf5c9a6f5fe
Jose Miguel <miguel.gamazo@galiclick.com>
2012-03-17 11:35:36 +0100

    Changed unicorn file

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,3 +7,11 @@ preload_app true
 listen "#{wd}/tmp/unicorn.todo.sock"
 worker_processes 2
 timeout 30
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
