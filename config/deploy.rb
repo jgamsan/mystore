33d7a9cbffabf5c435853bd48f6fb2b070b877c0
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-04-25 19:27:28 +0200

    Mejoras en deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
-set :rvm_ruby_string, 'ruby-1.9.3-p0'
+set :rvm_ruby_string, 'ruby-1.9.3-p125'
 set :rvm_type, :user
 require "bundler/capistrano"
 set :application, "mystore"
@@ -8,7 +8,7 @@ set :domain, "tienda.galiclick.com"
 set :user, "galiclick"
 set :port, 58378
 set :repository,  "git@github.com:jgamsan/mystore.git"
-
+set :keep_releases, 2
 
 set :scm, :git
 
@@ -21,10 +21,14 @@ role :app, domain
 role :db,  domain, :primary => true
 
 set :rails_env, "production"
-
-after 'deploy:update_code' do
-  run "cd #{release_path}; rake assets:precompile RAILS_ENV=production "
+namespace :customs do
+  task :symlink, :roles => :app do
+    run <<-CMD
+      ln -nfs #{shared_path}/system/spree #{release_path}/public
+    CMD
+  end
 end
 
+after "deploy:symlink","customs:symlink"
 after "deploy", "deploy:cleanup"
 require 'capistrano-unicorn'
